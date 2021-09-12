84ea0af49e70d42ac1c7f8096bf5c1c5e771efd5
Olivia Park <oliviaypark113@gmail.com>
2021-09-12 13:47:57 -0400

    topbar created

---
+++ client/src/components/Topbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './topbar.css'
+// import {Link} from "react-router-dom"
+
+export default function Topbar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+          <img src="../../assets/Logo.png" alt="castlewood logo"className="logo"/>
+          CASTLEWOOD ADVISORS
+      </div>
+      <div className="topCenter">
+      <ul className="topList">
+            <li className="topListItem">HOME</li>
+              <li className="topListItem">ABOUT</li>
+              <li className="topListItem">AWARDS</li>
+              <li className="topListItem">WHY ME?</li>
+              <li className="topListItem letstalk">LET'S TALK</li>
+          </ul>
+
+      </div>
+      <div className="topRight">
+      <img src="../../assets/david.jpg" alt="david"className="davidImg"/>
+      <i className="fas fa-bars topMenuIcon"></i>
+      </div>
+    </div>
+  )
+}
---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import { ContextProvider } from "./context/Context";
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <ContextProvider>
+      <App />
+    </ContextProvider>   */}
+    <App/>
+  </React.StrictMode>,
+    document.getElementById("root")
+  
+);
+
