f2931c9a91f20b47d2e2a3167d1dbda997389bfc
Olivia Park <oliviaypark113@gmail.com>
2021-09-12 21:40:03 -0400

    setup Setting page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Topbar from './components/Topbar';
 import Home from './pages/Home/Home';
+import Settings from './pages/Settings/Settings';
 
 
 function App() {
   return (
     <div className="app">
         <Topbar />
-        <Home />
+        {/* <Home /> */}
+        <Settings />
     </div>
   );
 }
---
+++ client/src/pages/Settings/Settings.jsx
@@ -0,0 +1,38 @@
+import "./settings.css"
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+          <div className="settingsTitle">
+              <span className="settingsUpdateTitle">
+                  Update Your Account
+              </span>
+              <span className="settingsDeleteTitle">
+                Delete Account
+              </span>
+          </div>
+
+          <form className="settingsForm">
+              <label>Profile Picture</label>
+
+              <div className="settingsPP">
+
+              <img src="../../../assets/david.jpg" alt="david_image"/>
+              <label htmlFor="fileInput">
+              <i className="fas fa-user settingsPPIcon"></i>
+              </label> 
+              <input type="file" id="fileInput" style={{display: "none"}}/>
+              </div>
+              <label>Username</label>
+              <input type="text" placeholder="Username"/>
+              <label>Email</label>
+              <input type="text" placeholder="Email"/>
+              <label>Password</label>
+              <input type="text" placeholder="Password"/>
+             <button className="settingsSubmit">Update</button>
+          </form>
+      </div>
+    </div>
+  )
+}
